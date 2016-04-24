c3ea02c3da957c06423096d46a95a6c2b201fe95
Tyler Berry <tyler@berryac.com>
2016-04-24 14:25:43 -0600

    this branch is purely docs now

---
+++ currency/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"struct":[["Currency","Represents currency through an optional symbol and amount of coin."],["ParseCurrencyError",""]]});
---
+++ implementors/core/fmt/trait.LowerExp.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['libc'] = [];implementors['currency'] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/fmt/trait.LowerExp.html' title='core::fmt::LowerExp'>LowerExp</a> for <a class='struct' href='currency/struct.Currency.html' title='currency::Currency'>Currency</a>",];implementors['currency'] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/fmt/trait.LowerExp.html' title='core::fmt::LowerExp'>LowerExp</a> for <a class='struct' href='currency/struct.Currency.html' title='currency::Currency'>Currency</a>",];implementors['currency'] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/fmt/trait.LowerExp.html' title='core::fmt::LowerExp'>LowerExp</a> for <a class='struct' href='currency/struct.Currency.html' title='currency::Currency'>Currency</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
