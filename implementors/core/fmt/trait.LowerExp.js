da6ad14b7f2b62d8e100722a0fbe29e12424762b
Tyler Berry <tyler@berryac.com>
2016-04-24 14:29:09 -0600

    updated docs

--- implementors/core/fmt/trait.LowerExp.js
+++ implementors/core/fmt/trait.LowerExp.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['libc'] = [];implementors['currency'] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/fmt/trait.LowerExp.html' title='core::fmt::LowerExp'>LowerExp</a> for <a class='struct' href='currency/struct.Currency.html' title='currency::Currency'>Currency</a>",];implementors['currency'] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/fmt/trait.LowerExp.html' title='core::fmt::LowerExp'>LowerExp</a> for <a class='struct' href='currency/struct.Currency.html' title='currency::Currency'>Currency</a>",];implementors['currency'] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/fmt/trait.LowerExp.html' title='core::fmt::LowerExp'>LowerExp</a> for <a class='struct' href='currency/struct.Currency.html' title='currency::Currency'>Currency</a>",];
+implementors['libc'] = [];implementors['currency'] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/fmt/trait.LowerExp.html' title='core::fmt::LowerExp'>LowerExp</a> for <a class='struct' href='currency/struct.Currency.html' title='currency::Currency'>Currency</a>",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
